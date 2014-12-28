00d901a9d87e670d0915daa753101957e118b19a
Zhi Han <zhi.han@gmail.com>
2014-12-28 11:04:33 -0500

    Added call to client api

--- static/js/auth.js
+++ static/js/auth.js
@@ -1,29 +1,40 @@
+// Constants
 var CLIENT_ID = "607994131901-if7as8ck330umlddcgmnmbhr05isalhm.apps.googleusercontent.com";
 var SCOPES = ["email"];
+var words = [];
 
+
+// Main callback from gapi
 function init() {
     var apisToLoad;
-    var loadCallback = function() {
+    function loadCallback() {
         if (--apisToLoad == 0) {
             signin(true, userAuthed);
         }
     };
-    
+
     apisToLoad = 2; // must match number of calls to gapi.client.load()
     apiRoot = '//' + window.location.host + '/_ah/api';
     gapi.client.load('wordsapi', 'v1', loadCallback, apiRoot);
     gapi.client.load('oauth2', 'v2', loadCallback);
 }
 
+// Sign in with google
 function signin(mode, authorizeCallback) {
     gapi.auth.authorize({client_id: CLIENT_ID,
-                         scope: SCOPES, immediate: mode},
+                        scope: SCOPES, immediate: mode},
                         authorizeCallback);
 }
 
+
 function userAuthed() {
-    function getUserEmail() {
-        var req = gapi.clien
+    function initializeWords() {
+        gapi.client.wordsapi.getallmywords().then(function(resp){
+            if (resp.result.hasOwnProperty('records')) {
+                words = resp.result.records;
+                main.data = words;
+            }
+        });
     }
 
     var request =
@@ -31,7 +42,9 @@ function userAuthed() {
             if (!resp.code) {
                 // User is signed in, call my Endpoint
                 $("#signinButton").addClass("hidden");
-                 $("#userinfo").text(resp.email);
+                $("#userinfo").text(resp.email);
+                // Initialize data
+                initializeWords();
             }
         });
 }
